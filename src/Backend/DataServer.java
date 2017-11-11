6effc44255680ab51e2910bc56b2d9d6577fab16
Patrick Young <tua43500@temple.edu>
2017-11-10 21:33:10 -0500

    cleaning

--- src/Backend/DataServer.java
+++ src/Backend/DataServer.java
@@ -2,7 +2,7 @@ package Backend;
 
 //talks to mySQL DB
 //getAllEvents returns event arraylist
-//getOneEvent- primary key needs to be a hash of the easily repeatable event fields, sDay,eDay,sTime,eTime
+//getOneEvent- primary key needs to be a hash of the easily repeatable event fields, sDay,eDay,sTime,eTime etc
 //             if pk is autoincrement client would have to pull all events to find the one it wants
 //saveEvent(event e)
 //searchOpening(event e) - fill event with numerics, the strings can be blank
