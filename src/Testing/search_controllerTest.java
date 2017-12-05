77baa138aed97803a424411a3d7ca73fc22bf826
Patrick Young <tua43500@temple.edu>
2017-12-05 14:10:47 -0500

    NONPRODUCTION COMMIT, changing workstations, ignore this

--- src/Testing/search_controllerTest.java
+++ src/Testing/search_controllerTest.java
@@ -12,13 +12,16 @@ import java.time.LocalTime;
 import static org.junit.Assert.*;
 
 public class search_controllerTest {
-    private boolean[][] results;
+    private boolean[][] results, results2;
     @Before
     public void setup(){
         results=new boolean[7][24];
+        results2=new boolean[7][24];
         for(int i=0; i<7;i++)
-            for(int j=0;j<24;j++)
+            for(int j=0;j<24;j++) {
+                results[i][j] = true;
                 results[i][j]=true;
+            }
     }
     @Test
     public void eliminate_by_type_test(){
@@ -37,9 +40,14 @@ public class search_controllerTest {
         flags[6]=false;
         pb.setDays(flags);
         pb.setStart(LocalTime.parse("09:00"));
-        pb.setEnd(LocalTime.parse("03:00"));
+        pb.setEnd(LocalTime.parse("15:00"));
         userPrefs prefs=pb.createPrefs();
         DataServer.savePrefs(prefs);
+
+        /*
+                Workdays M T W R F
+                from 9-15:00
+         */
         results=eliminate_by_eType(results,1);
         for(int i=0;i<24;i++){
             assertEquals(results[0][i],false);
@@ -70,6 +78,10 @@ public class search_controllerTest {
 
     @Test
     public void test_eliminate_by_conflict(){
+        results=new boolean[7][24];
+        for(int i=0; i<7;i++)
+            for(int j=0;j<24;j++)
+                results[i][j]=true;
         destroyDB();
         DataServer.init();
         eventBuilder eb = new eventBuilder();
@@ -78,7 +90,7 @@ public class search_controllerTest {
 
         eb.setName("testEvent3");
         eb.setsDay(LocalDate.parse("2017-12-05"));
-        eb.setsDay(LocalDate.parse("2017-12-05"));
+        eb.seteDay(LocalDate.parse("2017-12-05"));
         eb.setsTime(LocalTime.parse("01:00"));
         eb.seteTime(LocalTime.parse("06:00"));
         testEvent3=eb.createEvent();
@@ -100,7 +112,6 @@ public class search_controllerTest {
         eb.setName("testEvent6");
         testEvent6=eb.createEvent();
 
-        int event_length=120;
         LocalDate current_day=LocalDate.parse("2017-12-04");
         LocalTime current_time=LocalTime.parse("08:00");
         int currHour=current_time.getHour();
@@ -114,14 +125,17 @@ public class search_controllerTest {
 
         for(int i=0;i<24;i++) {
             if ((i <= 6 && i >= 1) || (i <= 18 && i >= 12)) {
+                System.out.printf("Checking off limits indexes, [0][%d] and [1][%d]", i);
                 assertEquals(false, results[0][i]);
                 assertEquals(false, results[1][i]);
             } else {
+                System.out.printf("Checking acceptable indexes, [0][%d] and [1][%d]", i);
                 assertEquals(true, results[0][i]);
                 assertEquals(true, results[1][i]);
             }
         }
         for(int i=0;i<currHour+2;i++){
+            System.out.printf("Checking time that has already passed+2 hours, [0][%d] and [1][%d]",i);
             assertEquals(false, results[0][i]);
             assertEquals(false, results[1][i]);
         }
@@ -156,10 +170,10 @@ public class search_controllerTest {
             //modify results for a personal event type search
             else if(type==2){
                 for(int i = 0; i< Array.getLength(work_days); i++){
-                    //if its not a work day, eliminate that whole day
-                    if(work_days[i]==true)
+                    //if its not a work day, keep the whole day
+                    if(work_days[i]==false)
                         for(int j=0;j<24;j++)
-                            results[i][j]=false;
+                            results[i][j]=true;
                     else{
                         for(int j=0;j<wStart;j++)
                             results[i][j]=true;
@@ -174,9 +188,47 @@ public class search_controllerTest {
         return results;
     }
 
+    /* Iterate through events
+    1. if its the same year, continue
+    2. if the event occurs within the 7 day window from [today,today+7], continue
+    3. get length of event, and the offset mapping the events start day onto our window interval
+    4. set eLength indexes in results to false at the correct offset
+ */
+    //TODO debug this
     private boolean[][] eliminate_by_conflicts(boolean [][] results, LocalDate today, LocalTime currTime){
+        Event[] events=DataServer.getAllEvent();
+        int window_start, window_end, currYear, eLength, eOffset;
 
+        window_start=today.getDayOfYear();
+        window_end=today.getDayOfYear()+7;
+        currYear=today.getYear();
 
+        //eliminate hours from today that have passed, as well as the next 2 hours to give a buffer
+        for(int i=0;i<currTime.getHour()+2;i++)
+            results[0][i]=false;
+
+        for(int i=0;i<Array.getLength(events);i++){
+            if(events[i].getsDay().getYear()==currYear){
+                //if it's in the desired window, examine more closely
+                if(events[i].getsDay().getDayOfYear()>=window_start && events[i].getsDay().getDayOfYear()<window_end){
+                    eLength=events[i].geteTime().getHour()-events[i].getsTime().getHour();//truncate minutes and compute length
+
+                    //in cases where event does not start on the hour, add additional hour to avoid deeper analysis
+                    if(events[i].getsTime().getMinute()!=0 || events[i].geteTime().getMinute()!=0) {
+                        System.out.println("DBG incrementing to compensate for partial hours");
+                        eLength++;
+                    }
+                    //for events that are less then 1 hour long, block the entire hour
+                    if(eLength<1) {
+                        System.out.println("DBG eLength<1, setting equal to 1");
+                        eLength = 1;
+                    }
+                    eOffset=window_end-events[i].getsDay().getDayOfYear();
+                    for(int j=eLength+events[i].getsTime().getHour(); j>events[i].getsTime().getHour();j--)
+                        results[eOffset][j]=false;
+                }
+            }
+        }
         return results;
     }
     //testing method only
